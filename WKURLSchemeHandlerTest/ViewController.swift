933fde1e3cba2f02e7e980fdfeca343ef279cb1e
atecle <adam.tecle@gmail.com>
2019-07-09 15:34:08 -0400

    Initial Commit

---
+++ WKURLSchemeHandlerTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WKURLSchemeHandlerTest
+//
+//  Created by Tecle, Adam on 7/9/19.
+//  Copyright © 2019 Tecle, Adam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ WKURLSchemeHandlerTestTests/WKURLSchemeHandlerTestTests.swift
@@ -0,0 +1,34 @@
+//
+//  WKURLSchemeHandlerTestTests.swift
+//  WKURLSchemeHandlerTestTests
+//
+//  Created by Tecle, Adam on 7/9/19.
+//  Copyright © 2019 Tecle, Adam. All rights reserved.
+//
+
+import XCTest
+@testable import WKURLSchemeHandlerTest
+
+class WKURLSchemeHandlerTestTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
